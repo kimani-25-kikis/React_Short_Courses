398765412103fb56cabd5504e86a5d58bae768e5
kimani-25-kikis <Kimanikikis@gmail.com>
2024-10-03 08:57:02 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <header>
+            <hi>My Website</hi>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Services</a></li>
+                   
+                </ul>
+                <hr />
+            </nav>
+        </header>
+    );
+}
+export default Header
